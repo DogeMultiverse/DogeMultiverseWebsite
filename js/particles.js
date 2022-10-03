59c2e75ecde44d62136ce617c225f0935c3b5387
axaxax <67626131+alexpvpmindustry@users.noreply.github.com>
2022-10-03 13:15:37 +0800

    new particles

--- js/particles.js
+++ js/particles.js
@@ -1,8 +1,8 @@
-var canvas = document.querySelector("#scene"),
+var canvas = document.querySelector("#sceneparticles"),
   ctx = canvas.getContext("2d"),
   particles = [],
   amount = 0,
-  mouse = {x:0,y:0},
+  mouse = {x:1000,y:1000},
   radius = 3;
 
 var colors = ["#FFFFFF","#FFFFF0", "#FFFFD0","#FFFFE0", "#FFFFAF"];
@@ -12,18 +12,18 @@ var copy = "Doge Multiverse";// document.querySelector("#copy");
 var ww = canvas.width = window.innerWidth;
 var wh = canvas.height = window.innerHeight; 
 function Particle(x,y){
-  this.x =  Math.random()*ww;
-  this.y =  Math.random()*wh;
+  this.x =  (10*(Math.random()-0.5))+x;
+  this.y =  (300*(Math.random()-0.5))+y;
   this.dest = {
     x : x,
     y: y
   };
-  this.r =  Math.random()*0.5 + 6;
+  this.r =  Math.random()*0.5 + 7;
   this.vx = (Math.random()-0.5)*30;
   this.vy = (Math.random()-0.5)*30;
   this.accX = 0;
   this.accY = 0;
-  this.friction =  0.1991975974;//Math.random()*0.05 +
+  this.friction =  0.23991975974;//Math.random()*0.05 +
 
   this.color = colors[Math.floor(Math.random()*6)];
 }
@@ -35,8 +35,8 @@ Particle.prototype.render = function() {
 
   this.vx += this.accX -this.vx*this.friction;
   this.vy += this.accY-this.vy*this.friction;
-  this.x += this.vx+Math.random()*0.95;
-  this.y +=  this.vy+Math.random()*0.95;
+  this.x += this.vx+Math.random()*1.95;
+  this.y +=  this.vy+Math.random()*1.95;
 
   ctx.fillStyle = this.color;
   ctx.beginPath();
@@ -48,8 +48,8 @@ Particle.prototype.render = function() {
 
   var distance2 = ( a*a + b*b );
   if(distance2<((radius*70)*(radius*70))){
-    this.accX = (this.x - mouse.x)/10;
-    this.accY = (this.y - mouse.y)/10;
+    this.accX = (this.x - mouse.x)/80;
+    this.accY = (this.y - mouse.y)/80;
     this.vx += this.accX;
     this.vy += this.accY;
   }
@@ -81,8 +81,8 @@ function initScene(){
 
   ctx.font = "bold "+(ww/10)+"px sans-serif";
   ctx.textAlign = "center";
-  ctx.transform(1,0,0,8,0,0);
-  ctx.fillText(copy, ww/2, wh/12);
+  ctx.transform(1,0,0,5,0,0);
+  ctx.fillText(copy, ww/2, wh/8);
   
   var data  = ctx.getImageData(0, 0, ww, wh).data;
   ctx.setTransform(1,0,0,1,0,0);
@@ -90,10 +90,10 @@ function initScene(){
   ctx.globalCompositeOperation = "screen";
 
   particles = [];
-  scale=120;
-  for(var i=0;i<ww;i+=Math.round(ww/scale)){
-    for(var j=0;j<wh;j+=Math.round(wh/scale)){
-      if(data[ ((i + j*ww)*4) + 3] > 10){
+  scale=50;
+  for(var i=0;i<ww;i+=Math.round(ww/scale/3)){
+    for(var j=0;j<wh;j+=Math.round(wh/scale/1.5)){
+      if(data[ ((i + j*ww)*4) + 3] > 10){ 
         particles.push(new Particle(i,j));
       }
     }
